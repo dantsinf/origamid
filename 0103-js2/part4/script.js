6aba98f96314b15ae6f3cd24fb3eabc5c86dcd19
Dante <39998160+LUlguim@users.noreply.github.com>
2021-08-17 20:07:09 -0300

    Add files via upload

---
+++ 0103-js2/part4/script.js
@@ -0,0 +1,22 @@
+//Arrays (Map e Filter)
+//Métodos para iterarmos entre os valores de arrays.
+
+const precos = [
+  'Crédito',
+  'R$ 200',
+  'R$ 400',
+  'Contas Pagar',
+  'R$ 300',
+  'R$ 400',
+  'Meus dados',
+];
+
+// Retorna uma array nova, que contem os items em
+// que o retorno da função for verdadeiro
+const precosFiltro = precos.filter((preco) => preco.includes('R$'));
+
+// Retorna uma nova array, modificada com o
+// retorno de cada item da função
+const precoNumeros = precosFiltro.map((preco) =>
+  Number(preco.replace('R$ ', '')),
+);
