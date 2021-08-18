a646578ca62f0f360c09ff4d329ff33e495ba507
Dante <contatocomdante@gmail.com>
2021-08-18 15:58:59 -0300

    começo do form, aqui um form reativa com o html

---
+++ 0401-form-input/README.md
@@ -0,0 +1,4 @@
+# 0401-form-input
+
+### Tela: mostra um formulário de criação de usuário
+### Fetch, condição nos botões, spread adiciona valores no form
---
+++ 0401-form-input/src/App.js
@@ -0,0 +1,223 @@
+import React from 'react';
+
+// ######## FORMULÁRIOS Input
+/*Reatividade
+Para criarmos campos de formulário reativos, devemos definir o estado para o value e a função atualizadora para o onChange.*/
+
+/// tela: uma input reativa com string
+/*
+const App = () => {
+  const [nome, setNome] = React.useState('');
+
+  return (
+    <form>
+      <label htmlFor="nome">Nome</label>
+      <input
+        type="text"
+        id="nome"
+        value={nome}
+        onChange={(event) => setNome(event.target.value)}
+      />
+      <p>{nome}</p>
+    </form>
+  );
+};
+*/
+
+/* ########## Form
+No form controlamos o que acontece ao enviar o mesmo, por isso definimos uma função para lidar com o onSubmit. O preventDefault() irá prevenir o comportamento padrão, que seria de atualizar a página, enviando uma requisição para o que estiver em action="".*/
+
+//Tela: formulário que mostra o valor no console
+//acima fizemos sem function
+/*
+const App = () => {
+  const [nome, setNome] = React.useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(nome);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="nome">Nome</label>
+      <input
+        type="text"
+        id="nome"
+        value={nome}
+        onChange={(event) => setNome(event.target.value)}
+      />
+      <button>Enviar</button>
+    </form>
+  );
+};
+*/
+
+/*########### Múltiplos Campos
+Podemos definir um estado para cada campo.*/
+
+//Tela: 2 input, 1x nome e 1x email
+// as strings do form após clicar no botão vão para o console.log
+/*
+const App = () => {
+  const [nome, setNome] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  function handleSubmit(event) {
+    //preventDefault() cancela o comportamento que uma tag tem
+    //é aplicado quando queremos colocar uma tag por motivos sde semântica
+    event.preventDefault();
+    console.log(nome, email);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="nome">Nome</label>
+      <input
+        type="text"
+        id="nome"
+        value={nome}
+        onChange={(event) => setNome(event.target.value)}
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <button>Enviar</button>
+    </form>
+  );
+};
+*/
+
+/*########## Objeto
+Podemos definir um objeto que irá conter todos os valores dos campos do formulário.*/
+
+/*
+const App = () => {
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: '',
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(form);
+  }
+
+  //o spread para adicionar o valor de value
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="nome">Nome</label>
+      <input type="text" id="nome" value={form.nome} onChange={handleChange} />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        value={form.email}
+        onChange={handleChange}
+      />
+      <button>Enviar</button>
+    </form>
+  );
+};
+*/
+
+//Tela: form para usuário preencher e após clicar no botão, string usuário criado
+const App = () => {
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: '',
+    senha: '',
+    cep: '',
+    rua: '',
+    numero: '',
+    bairro: '',
+    cidade: '',
+    estado: '',
+  });
+
+  const [response, setResponse] = React.useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    }).then((response) => {
+      setResponse(response);
+    });
+  }
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="nome">Nome</label>
+      <input type="text" id="nome" value={form.nome} onChange={handleChange} />
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        value={form.email}
+        onChange={handleChange}
+      />
+      <label htmlFor="senha">Senha</label>
+      <input
+        type="password"
+        id="senha"
+        value={form.senha}
+        onChange={handleChange}
+      />
+      <label htmlFor="cep">Cep</label>
+      <input type="text" id="cep" value={form.cep} onChange={handleChange} />
+      <label htmlFor="senha">Rua</label>
+      <input type="text" id="rua" value={form.rua} onChange={handleChange} />
+      <label htmlFor="numero">Número</label>
+      <input
+        type="text"
+        id="numero"
+        value={form.numero}
+        onChange={handleChange}
+      />
+      <label htmlFor="bairro">Bairro</label>
+      <input
+        type="text"
+        id="bairro"
+        value={form.bairro}
+        onChange={handleChange}
+      />
+      <label htmlFor="cidade">Cidade</label>
+      <input
+        type="text"
+        id="cidade"
+        value={form.cidade}
+        onChange={handleChange}
+      />
+      <label htmlFor="estado">Estado</label>
+      <input
+        type="text"
+        id="estado"
+        value={form.estado}
+        onChange={handleChange}
+      />
+      <button>Enviar</button>
+      {response && response.ok && <p>Usuário Criado</p>}
+    </form>
+  );
+};
+
+export default App;
