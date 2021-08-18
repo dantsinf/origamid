7e1f902e2ad291c2114475745e7bf1b4a0f7dd17
Dante <contatocomdante@gmail.com>
2021-08-17 21:35:29 -0300

    Hooks

---
+++ 0301-useState1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Produtos from './Produtos';
+
+const App = () => {
+  return (
+    <>
+      <button>Tablet</button>
+      <button>Smartphone</button>
+      <button>Notebook</button>
+    </>
+  );
+};
+
+export default App;
