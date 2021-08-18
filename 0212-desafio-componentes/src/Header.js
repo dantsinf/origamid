68d2e7c6fdc3889593a1901f0e1084974209359f
Dante <contatocomdante@gmail.com>
2021-08-17 21:18:35 -0300

    Atividade de final de modulo

---
+++ 0212-desafio-componentes/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <a href="/home">Home</a>
+        <br />
+        <a href="/produtos">Produtos</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ 0212-desafio-componentes/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Produtos from './Produtos';
+
+ReactDOM.render(
+  //rodando o react no modo estrito
+  // <React.StrictMode>
+  <App />,
+  //</React.StrictMode>,
+  document.getElementById('root'),
+);
