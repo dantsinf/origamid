967f671d4fbb0e6164e1a148b6bb3ea640b433e8
Dante <contatocomdante@gmail.com>
2021-08-20 18:05:55 -0300

    aula 0411-fizemos um custom Hook

---
+++ 0411-useform/README.md
@@ -0,0 +1,5 @@
+# 0411-useForm
+
+### Um custom hooks chamado useForm somente para tratar a validação do formulário
+
+### Tela: Input CEP com a validação e botão
---
+++ 0411-useform/src/Form/Input.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// FORMULÁRIOS, Componentes Input
+/*Input
+Podemos definir um componente para cada tipo de campo de formulário, assim evitamos criar código repetido.*/
+
+const Input = ({
+  id,
+  label,
+  value,
+  type,
+  onChange,
+  error,
+  onBlur,
+  placeholder,
+}) => {
+  return (
+    <>
+      <label htmlFor={id}>{label}</label>
+      <input
+        type={type}
+        id={id}
+        name={id}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        placeholder={placeholder}
+      />
+      {error && <p>{error}</p>}
+    </>
+  );
+};
+
+export default Input;
