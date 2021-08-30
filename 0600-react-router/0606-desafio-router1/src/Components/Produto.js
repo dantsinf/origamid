7f215e114e667d656dda2b278ee1c78cb7a21467
Dante <layscontato@gmail.com>
2021-08-30 15:03:09 -0300

    atualizacao loading

--- 0600-react-router/0606-desafio-router1/src/Components/Produto.js
+++ 0600-react-router/0606-desafio-router1/src/Components/Produto.js
@@ -1,24 +1,23 @@
 import React from 'react';
+import styles from './Produto.module.css';
 import { useParams } from 'react-router-dom';
 import Head from './Head';
-import styles from './Produto.module.css';
-import Produtos from './Produtos';
 
 const Produto = () => {
   const [produto, setProduto] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
-  const [error, setErro] = React.useState(null);
+  const [error, setError] = React.useState(null);
   const { id } = useParams();
 
   React.useEffect(() => {
     async function fetchProduto(url) {
       try {
         setLoading(true);
-        const reponse = await fetch(url);
-        const json = await reponse.json();
+        const response = await fetch(url);
+        const json = await response.json();
         setProduto(json);
-      } catch (error) {
-        setErro('Uma erro ocorreu');
+      } catch (erro) {
+        setError('Uma erro ocorreu');
       } finally {
         setLoading(false);
       }
@@ -26,24 +25,25 @@ const Produto = () => {
     fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
   }, [id]);
 
-  if (loading) return <div>Carregando...</div>;
+  if (loading) return <div className="loading">.</div>;
   if (error) return <p>{error}</p>;
   if (produto === null) return null;
   return (
     <section className={styles.produto + ' animeLeft'}>
       <Head
-        title={`Renak | ${produto.nome}`}
-        description={`Renak | Esse é um produto: ${produto.nome}`}
+        title={`Ranek | ${produto.nome}`}
+        description={`Ranek | Esse é um produto: ${produto.nome}`}
       />
-      {produto.fotos.map((foto) => (
-        <img key={foto.src} src={foto.src} alt={foto.titulo} />
-      ))}
+      <div>
+        {produto.fotos.map((foto) => (
+          <img key={foto.src} src={foto.src} alt={foto.titulo} />
+        ))}
+      </div>
       <div>
         <h1>{produto.nome}</h1>
         <span className={styles.preco}>R$ {produto.preco}</span>
         <p className={styles.descricao}>{produto.descricao}</p>
       </div>
-      <h1>Produto</h1>
     </section>
   );
 };
