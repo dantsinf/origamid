b20da24a1802ddeffef6dc6d093d7044fb56181c
Dante <layscontato@gmail.com>
2021-08-30 12:05:41 -0300

    atualizacao api, style

--- 0600-react-router/0606-desafio-router1/src/App.js
+++ 0600-react-router/0606-desafio-router1/src/App.js
@@ -13,6 +13,7 @@ const App = () => {
         <div className="content">
           <Routes>
             <Route path="/" element={<Produtos />} />
+            <Route path="produto/:id" element={<Produto />} />
             <Route path="contato" element={<Contato />} />
           </Routes>
         </div>
--- 0600-react-router/0606-desafio-router1/src/Components/Produtos.js
+++ 0600-react-router/0606-desafio-router1/src/Components/Produtos.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Produtos.module.css';
 
 const Produtos = () => {
+  const [produtos, setProdutos] = React.useState(null);
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+      .then((r) => r.json())
+      .then((json) => setProdutos(json));
+  }, []);
+  console.log(produtos);
+  if (produtos === null) return null;
   return (
-    <div className={styles.produtos}>
-      <h1>Produtos</h1>
-    </div>
+    <section className={styles.produtos + ' animeLeft'}>
+      {produtos.map((produto) => (
+        <Link to={`produto/${produto.id}`} key={produto.id}>
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
+          <h1 className={styles.nome}>{produto.nome}</h1>
+        </Link>
+      ))}
+    </section>
   );
 };
 
